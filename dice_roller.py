4952ec6bb7f2f61f5b68ecaea9cc5a36c9d636eb
sol1729 <102341329+sol1729@users.noreply.github.com>
2022-03-25 18:01:19 -0400

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -4,8 +4,13 @@ def main():
   sum = 0
   for i in range(0,die_rolls):
     roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
     sum += roll
+    if roll == 1:
+      print(f'You rolled a {roll}! Crtical Failure')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success')
+    else :
+      print(f'You rolled a {roll}')
   print(f'Sum of rolls {sum}')
 
 if __name__== "__main__":
